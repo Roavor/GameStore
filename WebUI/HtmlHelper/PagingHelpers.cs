d977f2ac5dfc4626158b26b3aef93cf4809865cb
Roavor <shpacovicho@gmail.com>
2017-09-26 21:55:06 +0300

    PagingHelper added

---
+++ WebUI/HtmlHelper/PagingHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+            PagingInfo pagingInro,Func<int,string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i <= pagingInro.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i==pagingInro.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn-primary");
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
