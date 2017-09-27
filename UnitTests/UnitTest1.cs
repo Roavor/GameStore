40c0e330c9d259ba8d89daf3d15730572405cc41
Roavor <shpacovicho@gmail.com>
2017-09-27 21:39:32 +0300

    add pages in site

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -7,6 +7,7 @@ using Moq;
 using Domain.Abstract;
 using Domain.Entities;
 using WebUI.Controllers;
+using WebUI.Models;
 
 namespace UnitTests
 {
@@ -27,12 +28,33 @@ namespace UnitTests
             });
             GameController controller = new GameController(mock.Object);
             controller.pageSize = 3;
-            IEnumerable<Game> result = (IEnumerable<Game>)controller.List(2).Model;
+            GamesListViewModel result =(GamesListViewModel)controller.List(2).Model;
 
-            List<Game> games = result.ToList();
+            List<Game> games = result.Games.ToList();
             Assert.IsTrue(games.Count == 2);
             Assert.AreEqual(games[0].Name, "Игра4");
             Assert.AreEqual(games[1].Name, "Игра5");
+        }
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameId = 1, Name = "Игра1"},
+                new Game { GameId = 2, Name = "Игра2"},
+                new Game { GameId = 3, Name = "Игра3"},
+                new Game { GameId = 4, Name = "Игра4"},
+                new Game { GameId = 5, Name = "Игра5"}
+            });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
+            GamesListViewModel result=(GamesListViewModel)controller.List(2).Model;
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
